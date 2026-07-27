5e29ec0a0ba531c5c0647c3293e00eed067b2271
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-27 14:29:46 -0700

    Add files via upload

---
+++ the2.py
@@ -0,0 +1,41 @@
+import random
+import streamlit as st 
+if 'num' not in st.session_state:
+  st.session_state.num
+if 'sc' not in st.session_state:
+  st.session_state.sc=0
+if 'count' not in st.session_state:
+  st.session_state.count=0
+if 'num1' not in st.session_state:
+ st.session_state.num1=random.randint(1,20)
+ st.session_state.num2=random.randint(1,20)
+ st.session_state.sign=random.choice(['+','-','*','/'])
+num1=st.session_state.num1
+num2=st.session_state.num2
+sign=st.session_state.sign
+if sign=='+':
+ sc=num1+num2
+if sign=='-':
+ sc=num1-num2
+if sign=='*':
+ sc=num1*num2
+if sign=='/':
+ sc=num1/num2
+st.title("أهلا بك في لعبتي ")
+st.write(num1,sign,num2)
+number=st.number_input("ادخل النتيجه ")
+if number == sc:
+    st.success("اجابتك صحيحه ! لقد احسنت")
+    st.session_state.num += 1
+    del st.session_state.num1
+    del st.session_state.num2
+    del st.session_state.sign
+    st.rerun()
+else:
+    st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
+    st.session_state.num = 0
+    del st.session_state.num1
+    del st.session_state.num2
+    del st.session_state.sign
+    st.rerun()
+st.write("your points are " ,st.session_state.num,"from",st.session_state.count,"Questions")
